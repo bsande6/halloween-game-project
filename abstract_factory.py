01a9e2918d8532ef341e066e8459bb1b14611c5e
bsande6 <bsande6@g.clemson.eu>
2022-11-20 21:20:06 -0500

    updating zombie factory structure

--- abstract_factory.py
+++ abstract_factory.py
@@ -8,15 +8,28 @@ class AbstractFactory(metaclass=abc.ABCMeta):
     def create_zombie(self):
         pass
 
-class BasicZombieFactory(AbstractFactory):
-    def create_zombie(self):
-        return BasicZombie()
+# class BasicZombieFactory(AbstractFactory):
+#     def create_zombie(self):
+#         return BasicZombie()
 
 
-class RunningZombieFactory(AbstractFactory):
-    def create_zombie(self):
-        return RunningZombie()
+# class RunningZombieFactory(AbstractFactory):
+#     def create_zombie(self):
+#         return RunningZombie()
 
-class RandomZombieFactory(AbstractFactory):
-    def create_zombie(self):
-        return RandomZombie()
+# class RandomZombieFactory(AbstractFactory):
+#     def create_zombie(self):
+#         return RandomZombie()
+
+
+# I think one factory is probably easier
+# I dont think we actually even need the abstract factory
+class ZombieFactory(AbstractFactory):
+    def create_zombie(self, zombie_type):
+        if zombie_type == "basic":
+            return BasicZombie()
+        elif zombie_type == "random":
+            return RandomZombie()
+        elif zombie_type == "running":
+            return RunningZombie()
+
